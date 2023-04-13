6f94ee1d350ee87e85a814138339fb562009bcec
programadornovato <info@programadornovato.com>
2023-04-13 03:39:27 -0600

    Contestar los WhatsApp con ChatGPT en PHP

--- envia.php
+++ envia.php
@@ -19,6 +19,45 @@ function enviar($recibido, $enviado, $idWA,$timestamp,$telefonoCliente) {
     }
     //SI LA CANTIDAD DE REGISTROS ES 0 ENVIAMOS EL MENSAJE DE LO CONTRARIO NO LO ENVIAMOS PORQUE YA SE ENVIO
     if ($cantidad == 0) {
+
+        $enviado=str_replace("\n","",$enviado);
+        //TOKEN QUE NOS DA FACEBOOK
+        $token = 'EAAIwtTEOYPABAOOqCEWfD0KcsuhSpjvbxSWEwuZCyYTJPb6XqrFMyFtvYoZAXLhG3FSNXJIUYREH9jw7RmvdpiWfwd7NW4bysXbMdgH0LElZAoMpJrQDopZBfrK6ZB1dZBXnPXHQLEkEMkhECWKObZC922m4SOkLwSi5NQVwwLwoVRgWnuM0PAukZAbVMkHQDIkuZAa4ZBTQEV6AZDZD';
+        //NUESTRO TELEFONO
+        $telefono = '527121122441';
+        //IDENTIFICADOR DE NÚMERO DE TELÉFONO
+        $telefonoID = '116907067953774';
+        //URL A DONDE SE MANDARA EL MENSAJE
+        $url = 'https://graph.facebook.com/v16.0/' . $telefonoID . '/messages';
+        //CONFIGURACION DEL MENSAJE
+        $mensaje = ''
+                . '{'
+                . '"messaging_product": "whatsapp", '
+                . '"recipient_type": "individual",'
+                . '"to": "' . $telefono . '", '
+                . '"type": "text", '
+                . '"text": '
+                . '{'
+                . '     "body":"' . $enviado . '",'
+                . '     "preview_url": true, '
+                . '} '
+                . '}';
+        //DECLARAMOS LAS CABECERAS
+        $header = array("Authorization: Bearer " . $token, "Content-Type: application/json",);
+        //INICIAMOS EL CURL
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $mensaje);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        //OBTENEMOS LA RESPUESTA DEL ENVIO DE INFORMACION
+        $response = json_decode(curl_exec($curl), true);
+        //OBTENEMOS EL CODIGO DE LA RESPUESTA
+        $status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        //CERRAMOS EL CURL
+        curl_close($curl);
+
+
         //$enviado= trim($enviado,"\n");
         $enviado=str_replace("\n","",$enviado);
         //INSERTAMOS LOS REGISTROS DEL ENVIO DEL WHATSAPP
